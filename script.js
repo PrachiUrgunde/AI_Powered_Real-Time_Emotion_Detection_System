4b4797fc9ce76c093332cd4899a62b682421812e
Mohini A Shrikhande <mohinishrikhande7@gmail.com>
2026-02-04 14:52:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple JavaScript for basic interactions
+
+// Console log for Start button
+document.getElementById("startBtn").addEventListener("click", () => {
+  alert("Starting emotion detection...");
+});
+
